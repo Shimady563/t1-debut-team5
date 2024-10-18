d7c6dc97cde2fc702dd7228f282e66e87fd9bed1
Maksim Shiryaev <shimady563@gmail.com>
2024-10-18 21:43:09 +0300

    feat: init backend project
    init spring project
    create docker compose for dev builds

---
+++ backend/src/test/java/com/team5/techradar/TechradarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.team5.techradar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechradarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
