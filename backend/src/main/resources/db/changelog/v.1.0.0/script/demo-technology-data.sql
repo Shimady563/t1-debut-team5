3e902ff644a91aae2f646b30736d4410cab812f1
Maksim Shiryaev <shimady563@gmail.com>
2024-10-19 22:28:29 +0300

    feat(migrations): add new migrations
    add migration for creating specialization table
    add migrations for populating tables with demo data

---
+++ backend/src/main/resources/db/changelog/v.1.0.0/script/demo-technology-data.sql
@@ -0,0 +1,12 @@
+INSERT INTO technology (id, name, moved, level, type)
+VALUES
+    (1, 'Java', 'NOT_MOVED', 'ADOPT', 'LANGUAGES'),
+    (2, 'JavaScript', 'UP', 'TRIAL', 'LANGUAGES'),
+    (3, 'PostgreSQL', 'UP', 'ADOPT', 'DATABASES'),
+    (4, 'Docker', 'NOT_MOVED', 'ASSESS', 'PLATFORMS'),
+    (5, 'AWS', 'UP', 'TRIAL', 'PLATFORMS'),
+    (6, 'Kubernetes', 'UP', 'ASSESS', 'PLATFORMS'),
+    (7, 'MongoDB', 'DOWN', 'HOLD', 'DATABASES'),
+    (8, 'Spring Boot', 'NOT_MOVED', 'ADOPT', 'LANGUAGES'),
+    (9, 'React', 'UP', 'TRIAL', 'LANGUAGES'),
+    (10, 'REST', 'NOT_MOVED', 'ASSESS', 'TOOLS');
