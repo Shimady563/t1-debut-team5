17ccf0b9018035712b0a94a6dce3a2e5bb54b36c
Maksim Shiryaev <shimady563@gmail.com>
2024-10-20 15:11:19 +0300

    feat: add data access exception handling

--- backend/src/main/java/com/team5/techradar/exception/GlobalExceptionHandler.java
+++ backend/src/main/java/com/team5/techradar/exception/GlobalExceptionHandler.java
@@ -1,10 +1,13 @@
 package com.team5.techradar.exception;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.dao.DataAccessException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.time.LocalDateTime;
 
 @Slf4j
 @ControllerAdvice
@@ -20,5 +23,24 @@ public class GlobalExceptionHandler {
                 ),
                 HttpStatus.NOT_FOUND
         );
+    }
+
+    @ExceptionHandler(DataAccessException.class)
+    public ResponseEntity<AppError> handleDataAccessException(DataAccessException e) {
+        log.error(e.getMessage());
+        return new ResponseEntity<>(
+                new AppError(
+                        buildDataExceptionMessage(e.getMostSpecificCause().getMessage()),
+                        HttpStatus.CONFLICT.value()
+                ),
+                HttpStatus.CONFLICT
+        );
+    }
+
+    private String buildDataExceptionMessage(String message) {
+        if (message.contains("Detail:")) {
+            return message.split("Detail:")[1].trim();
+        }
+        return message;
     }
 }
