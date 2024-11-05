30360f5b2fbb0c77559d9914e0a29448365a86d4
Shimady563 <shimady563@gmail.com>
2024-11-05 16:50:44 +0300

    build: configure kafka for integration

--- backend/src/main/java/com/team5/techradar/config/GenericConfig.java
+++ backend/src/main/java/com/team5/techradar/config/GenericConfig.java
@@ -1,15 +1,25 @@
 package com.team5.techradar.config;
 
 import com.team5.techradar.map.*;
+import org.apache.kafka.clients.admin.NewTopic;
 import org.modelmapper.ModelMapper;
 import org.modelmapper.convention.MatchingStrategies;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.config.TopicBuilder;
 
 @EnableCaching
 @Configuration
 public class GenericConfig {
+
+    @Bean
+    public NewTopic technologyTopic() {
+        return TopicBuilder.name("technologyTopic")
+                .partitions(4)
+                .replicas(1)
+                .build();
+    }
 
     @Bean
     public ModelMapper modelMapper() {
