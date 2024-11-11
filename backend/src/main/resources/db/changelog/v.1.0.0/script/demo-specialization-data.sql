37ddfc44f36ee4c7253e8afaece559d2a175f93e
Maksim Shiryaev <shimady563@gmail.com>
2024-11-11 19:00:07 +0300

    fix: fix specialization migrations

--- backend/src/main/resources/db/changelog/v.1.0.0/script/demo-specialization-data.sql
+++ backend/src/main/resources/db/changelog/v.1.0.0/script/demo-specialization-data.sql
@@ -1,13 +1,13 @@
 -- Администратор системы - специальная роль для администраторов и не подразумевается для выбора
 INSERT INTO specialization (id, name)
-VALUES (nextval('specialization_id_seq'), 'Backend-разработчик'),
+VALUES (nextval('specialization_id_seq'), 'Администратор системы'),
+       (nextval('specialization_id_seq'), 'Backend-разработчик'),
        (nextval('specialization_id_seq'), 'Frontend-разработчик'),
-       (nextval('specialization_id_seq'), 'Frontend-разработчик'),
+       (nextval('specialization_id_seq'), 'Data Scientist'),
        (nextval('specialization_id_seq'), 'Мобильный разработчик'),
        (nextval('specialization_id_seq'), 'DevOps-инженер'),
        (nextval('specialization_id_seq'), 'QA-инженер'),
-       (nextval('specialization_id_seq'), 'Инженер по автоматизации тестирования'),
-       (nextval('specialization_id_seq'), 'Системный аналитик'),
-       (nextval('specialization_id_seq'), 'Администратор системы');
+       (nextval('specialization_id_seq'), 'ML-Инженер'),
+       (nextval('specialization_id_seq'), 'Системный аналитик');
 
 
